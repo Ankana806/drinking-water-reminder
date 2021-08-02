9e0f6b6a7a30aa1cba40785a0807b7186f6cda31
Ankana806 <85381913+Ankana806@users.noreply.github.com>
2021-08-02 21:54:54 +0530

    Add files via upload

---
+++ project-18/main.py
@@ -0,0 +1,11 @@
+import time
+from plyer import notification
+while True:
+    if __name__=="__main__":
+        notification.notify(
+            title="**Please Drink Water Now**",
+            message="water! water! water!",
+            timeout=10
+        )
+        
+       time(60*60)
